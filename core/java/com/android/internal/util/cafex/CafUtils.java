bde7c4d65533d155d84639d3aaf53f6bfafc28ae
PMS22 <prathams99@rediff.com>
2018-10-16 02:25:57 +0530

    SystemUI: CompassTile: Add a device supports compass check
    - Previously it showed up on devices even without compass support
      making it a bloat
    
    Change-Id: Iba3b29e4be2c0fd5f97573c2d0abbde7ad1db995
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: NurKeinNeid <mralexman3000@gmail.com>
    Signed-off-by: SagarMakhar <sagarmakhar@gmail.com>

--- core/java/com/android/internal/util/cafex/CafUtils.java
+++ core/java/com/android/internal/util/cafex/CafUtils.java
@@ -37,6 +37,8 @@ import android.hardware.input.InputManager;
 import android.hardware.fingerprint.FingerprintManager;
 import android.provider.Settings;
 import android.os.AsyncTask;
+import android.hardware.Sensor;
+import android.hardware.SensorManager;
 import android.os.Handler;
 import android.os.Looper;
 import android.os.PowerManager;
@@ -214,6 +216,12 @@ public class CafUtils {
         } catch (RemoteException e) {
             e.printStackTrace();
         }
+    }
+
+    public static boolean deviceHasCompass(Context ctx) {
+        SensorManager sm = (SensorManager) ctx.getSystemService(Context.SENSOR_SERVICE);
+        return sm.getDefaultSensor(Sensor.TYPE_ACCELEROMETER) != null
+                && sm.getDefaultSensor(Sensor.TYPE_MAGNETIC_FIELD) != null;
     }
 
     private static final class FireActions {
