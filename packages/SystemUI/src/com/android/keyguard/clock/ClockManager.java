87b21c113515a78b7cb0c797f52ccb4cbef43fe4
rohan <purohit.rohan@gmail.com>
2020-07-14 10:05:57 -0400

    SystemUI: Add OnePlus Numbers clock
    * Inspired by oneplus aod clocks
      thanks to @ROGERdotT for the design.
    
    Change-Id: Ie4cf5f95787aac42d2c95eaf4afd59aa4a5f8247

--- packages/SystemUI/src/com/android/keyguard/clock/ClockManager.java
+++ packages/SystemUI/src/com/android/keyguard/clock/ClockManager.java
@@ -164,7 +164,8 @@ public final class ClockManager {
         addBuiltinClock(() -> new FluidClockController(res, layoutInflater, colorExtractor));
         addBuiltinClock(() -> new IDEClockController(res, layoutInflater, colorExtractor, context));
         addBuiltinClock(() -> new FluidClockController(res, layoutInflater, colorExtractor));
-
+        addBuiltinClock(() -> new OPNumbersClockController(res, layoutInflater, colorExtractor));
+        
         // Store the size of the display for generation of clock preview.
         DisplayMetrics dm = res.getDisplayMetrics();
         mWidth = dm.widthPixels;
