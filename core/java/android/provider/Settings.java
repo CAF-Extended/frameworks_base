8e719d21cd21b213265c723426e34a5036d255e8
Ido Ben-Hur <idoybh2@gmail.com>
2020-10-09 15:16:28 +0300

    base: Allow hiding notification headers [1/2]
    Gimme back my space, thank you
    
    Change-Id: I5f63a384d2bd2052d4a8a9d49aff1456d743367f
    Signed-off-by: Alexandru Scurtu <sasha.scurtu@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4893,6 +4893,12 @@ public final class Settings {
         public static final String ANIM_TILE_INTERPOLATOR = "anim_tile_interpolator";
 
         /**
+         * Controls whether to show notification headers
+         * @hide
+         */
+        public static final String NOTIFICATION_HEADERS = "notification_headers";
+
+        /**
          * Volume keys control cursor in text fields (default is 0)
          * 0 - Disabled
          * 1 - Volume up/down moves cursor left/right
