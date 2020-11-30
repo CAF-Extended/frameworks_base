eb2e2ac56284d97f1288f1b3266a1c5dec120bcc
Nate Myren <ntmyren@google.com>
2020-11-30 12:06:26 -0800

    [android-11.0.0_r37] Revoke storage on SDK downgrade or new full storage request
    When an app downgrades from above API 28 to below, or when an app begins
    requesting full storage in api 29, revoke storage permissions
    
    Test: Atest StorageEscalationTest
    Bug: 171430330
    Merged-In: Ia6bd5658b77a26ab15fea013ec875050457473e0
    Change-Id: Ia6bd5658b77a26ab15fea013ec875050457473e0
    (cherry picked from commit b29834bc15dc9de1a1b138741c3c0d4fa8169110)

--- services/core/java/com/android/server/pm/PackageManagerService.java
+++ services/core/java/com/android/server/pm/PackageManagerService.java
@@ -12560,6 +12560,7 @@ public class PackageManagerService extends IPackageManager.Stub
                     if (hasOldPkg) {
                         mPermissionManager.revokeRuntimePermissionsIfGroupChanged(pkg, oldPkg,
                                 allPackageNames);
+                        mPermissionManager.revokeStoragePermissionsIfScopeExpanded(pkg, oldPkg);
                     }
                     if (hasPermissionDefinitionChanges) {
                         mPermissionManager.revokeRuntimePermissionsIfPermissionDefinitionChanged(
