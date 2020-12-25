1101990ca7f2638efec390cc2d0f8979a9cb3caf
ezio84 <brabus84@gmail.com>
2020-12-25 21:47:24 +0100

    Hidden gestural bar: fix visual glitches when switching states
    hacky way but it works (TODO: find a proper fix in the new year,
    i don't have motivation to dig into navbar/keyguard code fuckery now lol)
    
    setting the height to 1px keeps the navbar almost invisible
    but fixes the annoying visual glitches when going from screen
    off to ambient pulsing or lockscreen (more noticeable on some devices
    like bonito)
    to replicate the issue without this commit:
    - screen ON, then screen off, then double tap to go to ambient,
    then double tap to go to lockscreen, then double tap to switch screen off,
    then switch screen on again
    - or just switch screen off/on a few times with the power button
    
    Also sync the hide pill code with Pulse hide pill feature
    
    Change-Id: Ib1cc83492f8a091be5cac4563d844010cef69dbc

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/NavigationBarView.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/NavigationBarView.java
@@ -737,8 +737,13 @@ public class NavigationBarView extends FrameLayout implements
         getBackButton().setVisibility(disableBack       ? View.INVISIBLE : View.VISIBLE);
         getHomeButton().setVisibility(disableHome       ? View.INVISIBLE : View.VISIBLE);
         getRecentsButton().setVisibility(disableRecent  ? View.INVISIBLE : View.VISIBLE);
-        getHomeHandle().setVisibility(disableHomeHandle || mHomeHandleForceHidden ? View.INVISIBLE : View.VISIBLE);
+        getHomeHandle().setVisibility(disableHomeHandle|| !isHintEnabled()
+                ? View.INVISIBLE : View.VISIBLE);
         notifyActiveTouchRegions();
+    }
+
+    public boolean isHintEnabled() {
+        return mNavigationInflaterView.isHintEnabled();
     }
 
     public void hideHomeHandle(boolean hide) {
@@ -955,7 +960,7 @@ public class NavigationBarView extends FrameLayout implements
     private void updateSamplingRect() {
         mSamplingBounds.setEmpty();
         // TODO: Extend this to 2/3 button layout as well
-        View view = getHomeHandle().getCurrentView();
+        View view = isHintEnabled() ? getHomeHandle().getCurrentView() : null;
 
         if (view != null) {
             int[] pos = new int[2];
