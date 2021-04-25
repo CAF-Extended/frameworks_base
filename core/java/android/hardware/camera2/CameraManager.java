4586576fb538a45ac67bd70b048b9c34a101d6ee
sb6596 <shubhamprince111@gmail.com>
2021-04-25 14:17:51 +0000

    [UPSTREAM]camera: Support exposing aux camera to apps
    * Replace vendor.camera.aux.packageblacklist with vendor.camera.aux.packageexcludelist
      as recommended by coding guidelines https://developers.google.com/style/word-list#blacklist.
    
    Original change:
    
    Author: Sauhard Pande <sauhardp@codeaurora.org>
    Date:   Thu Jul 21 18:23:21 2016 -0700
    
        Camera: Expose Aux camera to apps present in the whitelist
    
        Issue:
        3rd party apk cannot handle additional aux camera and may cause crash,
        Mono camera doesnt support all capabilities of HAL3 causing CTS issue.
    
        Fix:
        1. Expose aux camera to apps present in the whitelist
        2. ignore the availabe/unavailable status update for aux camera
        3. returning exception for open request for bad cameraid
    
        Change-Id: I15910154c6df205e6d4e00bfad30a00c9e3d5bee
        CRs-Fixed: 1086937
    
    ---------------------------------------------------------------------------
    
    Forward-ported to R and squashed with the following changes:
    
    Author: Rashed Abdel-Tawab <rashed@linux.com>
    Date:   Sat Mar 24 13:20:52 2018 -0400
    
        camera: Check if aux camera whitelist is set before restricting cameras
    
        Some devices have a fully featured 3rd camera and adding 20+ camera apps
        to the whitelist is impossible due to the string length limit on
        systemprops. Add a check to see if the prop is even set, and if not,
        check if the blacklist property is set and mark those apps to hide the
        3rd camera from. If a package is not part of the blacklist, just restore
        the original behaviour and expose all the cameras to the app.
    
        Change-Id: I6c3b33c077e8710c73b5d0fa28e1b017d6c43a58
    
    Author: Paul Keith <javelinanddart@gmail.com>
    Date:   Fri Feb 8 08:42:33 2019 -0600
    
        Camera: Simplify code for Aux/Mono white and black lists
    
        Change-Id: I20c5454385d4d830a9a4d61926807a7eb1407c95
    
    Change-Id: I15910154c6df205e6d4e00bfad30a00c9e3d5bee

--- core/java/android/hardware/camera2/CameraManager.java
+++ core/java/android/hardware/camera2/CameraManager.java
@@ -22,8 +22,8 @@ import android.annotation.Nullable;
 import android.annotation.RequiresPermission;
 import android.annotation.SystemService;
 import android.annotation.TestApi;
-import android.app.ActivityThread;
 import android.content.Context;
+import android.hardware.Camera;
 import android.hardware.CameraInfo;
 import android.hardware.CameraStatus;
 import android.hardware.ICameraService;
@@ -37,6 +37,7 @@ import android.hardware.camera2.utils.CameraIdAndSessionConfiguration;
 import android.hardware.camera2.utils.ConcurrentCameraIdCombination;
 import android.hardware.display.DisplayManager;
 import android.os.Binder;
+import android.app.ActivityThread;
 import android.os.DeadObjectException;
 import android.os.Handler;
 import android.os.IBinder;
@@ -44,13 +45,12 @@ import android.os.RemoteException;
 import android.os.ServiceManager;
 import android.os.ServiceSpecificException;
 import android.os.SystemProperties;
-import android.text.TextUtils;
-import android.util.Log;
 import android.util.ArrayMap;
 import android.util.ArraySet;
 import android.util.Log;
 import android.util.Size;
 import android.view.Display;
+import android.text.TextUtils;
 
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -1239,24 +1239,10 @@ public final class CameraManager {
 
         private String[] extractCameraIdListLocked() {
             String[] cameraIds = null;
-            boolean exposeAuxCamera = true;
-            String packageName = ActivityThread.currentOpPackageName();
-            String packageList = SystemProperties.get("vendor.camera.aux.packagelist", "");
-            String packageBlacklist = SystemProperties.get("vendor.camera.aux.packageblacklist", "");
-            if (!packageList.isEmpty()) {
-                exposeAuxCamera = false;
-                if (Arrays.asList(packageList.split(",")).contains(packageName)) {
-                    exposeAuxCamera = true;
-                }
-            } else if (!packageBlacklist.isEmpty()) {
-                exposeAuxCamera = true;
-                if (Arrays.asList(packageBlacklist.split(",")).contains(packageName)) {
-                    exposeAuxCamera = false;
-                }
-            }
+            boolean exposeAuxCamera = Camera.shouldExposeAuxCamera();
             int idCount = 0;
             for (int i = 0; i < mDeviceStatus.size(); i++) {
-                if(!exposeAuxCamera && (i == 2)) break;
+                if (!exposeAuxCamera && i == 2) break;
                 int status = mDeviceStatus.valueAt(i);
                 if (status == ICameraServiceListener.STATUS_NOT_PRESENT
                         || status == ICameraServiceListener.STATUS_ENUMERATING) continue;
@@ -1265,7 +1251,7 @@ public final class CameraManager {
             cameraIds = new String[idCount];
             idCount = 0;
             for (int i = 0; i < mDeviceStatus.size(); i++) {
-                if(!exposeAuxCamera && (i == 2)) break;
+                if (!exposeAuxCamera && i == 2) break;
                 int status = mDeviceStatus.valueAt(i);
                 if (status == ICameraServiceListener.STATUS_NOT_PRESENT
                         || status == ICameraServiceListener.STATUS_ENUMERATING) continue;
@@ -1737,30 +1723,9 @@ public final class CameraManager {
         }
 
         private void onStatusChangedLocked(int status, String id) {
-            /* Force to ignore the last mono/aux camera status update
-             * if the package name does not falls in this bucket
-             */
-            boolean exposeMonoCamera = true;
-            String packageName = ActivityThread.currentOpPackageName();
-            String packageList = SystemProperties.get("vendor.camera.aux.packagelist", "");
-            String packageBlacklist = SystemProperties.get("vendor.camera.aux.packageblacklist", "");
-            if (!packageList.isEmpty()) {
-                exposeMonoCamera = false;
-                if (Arrays.asList(packageList.split(",")).contains(packageName)) {
-                    exposeMonoCamera = true;
-                }
-            } else if (!packageBlacklist.isEmpty()) {
-                exposeMonoCamera = true;
-                if (Arrays.asList(packageBlacklist.split(",")).contains(packageName)) {
-                    exposeMonoCamera = false;
-                }
-            }
-
-            if (exposeMonoCamera == false) {
-                if (Integer.parseInt(id) >= 2) {
-                    Log.w(TAG, "[soar.cts] ignore the status update of camera: " + id);
-                    return;
-                }
+            if (!Camera.shouldExposeAuxCamera() && Integer.parseInt(id) >= 2) {
+                Log.w(TAG, "[soar.cts] ignore the status update of camera: " + id);
+                return;
             }
 
             if (DEBUG) {
