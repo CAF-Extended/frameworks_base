b0e77a66e7494f743986926840e533fc5d36050e
Sergey Vasiliev <sdv22@live.ru>
2021-06-19 11:04:49 +0300

    Fix RR, Perf,Therm profiles on screen mode change
    Change-Id: I33c29ab99d21a47595cd9d876f95d2f25031015a

--- core/java/com/android/internal/baikalos/AppProfileManager.java
+++ core/java/com/android/internal/baikalos/AppProfileManager.java
@@ -66,6 +66,7 @@ public class AppProfileManager extends MessageHandler {
     private int mTopUid=-1;
     private String mTopPackageName;
     private boolean mReaderMode;
+    private boolean mPhoneCall;
 
     private boolean mIdleProfileActive;
     private String mActivePerfProfile = "default";
@@ -96,7 +97,7 @@ public class AppProfileManager extends MessageHandler {
 
     @Override
     protected void initialize() {
-        if( Constants.DEBUG_APP_PROFILE ) Slog.i(TAG,"initialize()");                
+        if( BaikalConstants.BAIKAL_DEBUG_APP_PROFILE ) Slog.i(TAG,"initialize()");                
         synchronized(mLock) {
 
             mAppSettings = AppProfileSettings.getInstance(getHandler(), getContext(), getContext().getContentResolver(), null);
@@ -148,19 +149,22 @@ public class AppProfileManager extends MessageHandler {
     }
 
     protected void setActiveFrameRateLocked(int fps) {
+
+        if( BaikalConstants.BAIKAL_DEBUG_APP_PROFILE ) Slog.i(TAG,"setActiveFrameRateLocked :" + fps);
+
         int setfps = fps;
         if( fps == -1 ) {
             try {
-                int value = Integer.parseInt(SystemProperties.get("persist.baikal.fps.default","4"));
+                int value = Integer.parseInt(SystemProperties.get("persist.baikal.fps.default","6"));
                 if( value != mDefaultFps ) { 
                     mDefaultFps = value;
-                    if( Constants.DEBUG_APP_PROFILE ) Slog.i(TAG,"mDefaultFps changed=" + mDefaultFps);
+                    if( BaikalConstants.BAIKAL_DEBUG_APP_PROFILE ) Slog.i(TAG,"mDefaultFps changed=" + mDefaultFps);
                     setfps = mDefaultFps-1;
                 }
             } catch( Exception e ) {
-                if( Constants.DEBUG_APP_PROFILE ) Slog.i(TAG,"mDefaultFps:", e);
-                mDefaultFps = 4;
-                setfps = 3;
+                if( BaikalConstants.BAIKAL_DEBUG_APP_PROFILE ) Slog.i(TAG,"mDefaultFps:", e);
+                mDefaultFps = 6;
+                setfps = 5;
             }
         }
         //if( mActiveFrameRate != fps || setfps != fps ) {
@@ -175,9 +179,10 @@ public class AppProfileManager extends MessageHandler {
             mDeviceIdleMode = mode;
             Runtime.setIdleMode(mode);
             BaikalSettings.setIdleMode(mode);
-            if( !mScreenMode ) {
-                setIdlePerformanceMode(mode);
-            }
+            //if( !mScreenMode ) {
+            //    setIdlePerformanceMode(mode);
+            //} 
+            restoreProfileForCurrentMode();
         }
     }
 
@@ -191,16 +196,28 @@ public class AppProfileManager extends MessageHandler {
                          callState.getForegroundCallState() > 0 ||
                          callState.getBackgroundCallState() > 0;
 
-        if( state ) {
-            BaikalUtils.boost();
-        } 
+        if( mPhoneCall != state ) {
+            mPhoneCall = state;
+    
+            if( mPhoneCall ) {
+                BaikalUtils.boost();
+                setActivePerfProfileLocked("default");
+                setActiveThermProfileLocked("incall");
+            } 
+        }
     }
 
 
     protected void setScreenModeLocked(boolean mode) {
         if( mScreenMode != mode ) {
             mScreenMode = mode;
-            restoreProfileForCurrentMode();
+            if( BaikalConstants.BAIKAL_DEBUG_APP_PROFILE ) Slog.i(TAG,"Restore profile after screen mode changed mode=" + mScreenMode);
+            getHandler().postDelayed( new Runnable() {
+                @Override
+                public void run() { 
+                    restoreProfileForCurrentMode();
+                }
+            },500);
         }
     }
 
@@ -214,12 +231,15 @@ public class AppProfileManager extends MessageHandler {
 
     protected void restoreProfileForCurrentMode() {
         if( mScreenMode ) {
+            //setActiveFrameRateLocked(-1);
             if( getIdlePerformanceMode() ) {
                 setIdlePerformanceMode(false);
             }
-            setHwPerfProfileLocked(mActivePerfProfile, true);
-            setHwThermProfileLocked(mActiveThermProfile, true);
+            //setHwPerfProfileLocked(mActivePerfProfile, true);
+            //setHwThermProfileLocked(mActiveThermProfile, true);
+            activateCurrentProfileLocked();
         } else {
+            setActiveFrameRateLocked(0);
             if( Runtime.isIdleMode() /*&& BaikalSettings.getExtremeIdleEnabled() */) {
                 setIdlePerformanceMode(true);
             } else {
@@ -229,18 +249,21 @@ public class AppProfileManager extends MessageHandler {
         }
     }
 
-    protected void setTopAppLocked(int uid, String packageName) {
-        if( uid != mTopUid || packageName != mTopPackageName ) {
-            mTopUid = uid;
-            mTopPackageName = packageName;
-            mReaderModeAvailable  = SystemProperties.get("sys.baikal.reader", "1").equals("1");
+    protected void activateCurrentProfileLocked() {
 
-            BaikalSettings.setTopApp(mTopUid, mTopPackageName);
-
-            AppProfile profile = mAppSettings.getProfile(/*uid,*/packageName);
-            synchronized(mCurrentProfileSync) {
-                mCurrentProfile = profile;
+            if( !mScreenMode ) {
+                if( BaikalConstants.BAIKAL_DEBUG_APP_PROFILE ) Slog.i(TAG,"Can't activate profile screen mode is " + mScreenMode);
+                return;
             }
+
+
+            AppProfile profile = null;
+            synchronized(mCurrentProfileSync) {
+                profile = mCurrentProfile;
+            }
+
+            if( BaikalConstants.BAIKAL_DEBUG_APP_PROFILE ) Slog.i(TAG,"Activate current profile profile=" + profile);
+
             if( profile == null /*|| uid < Process.FIRST_APPLICATION_UID */) {
                 setReaderModeLocked(false);
                 setActivePerfProfileLocked("default");
@@ -257,6 +280,27 @@ public class AppProfileManager extends MessageHandler {
                 Actions.sendBrightnessOverrideChanged(setBrightnessOverrideLocked(profile.mBrightness));
                 setRotation(profile.mRotation-1);
             }
+    }
+
+    protected void setTopAppLocked(int uid, String packageName) {
+
+        if( packageName != null )  packageName = packageName.split(":")[0];
+
+        if( uid != mTopUid || packageName != mTopPackageName ) {
+            mTopUid = uid;
+            mTopPackageName = packageName;
+
+            if( BaikalConstants.BAIKAL_DEBUG_APP_PROFILE ) Slog.i(TAG,"topAppChanged uid=" + uid + ", packageName=" + packageName);
+
+            mReaderModeAvailable  = SystemProperties.get("sys.baikal.reader", "1").equals("1");
+            BaikalSettings.setTopApp(mTopUid, mTopPackageName);
+
+            AppProfile profile = mAppSettings.getProfile(/*uid,*/packageName);
+            synchronized(mCurrentProfileSync) {
+                mCurrentProfile = profile;
+            }
+
+            activateCurrentProfileLocked();
         }
     }
 
@@ -286,7 +330,6 @@ public class AppProfileManager extends MessageHandler {
                 String action = intent.getAction();
                 String packageName = (String)intent.getExtra(Actions.EXTRA_PACKAGENAME);
                 int uid = (int)intent.getExtra(Actions.EXTRA_UID);
-                if( Constants.DEBUG_APP_PROFILE ) Slog.i(TAG,"topAppChanged uid=" + uid + ", packageName=" + packageName);
                 setTopAppLocked(uid,packageName);
             }
         }
@@ -298,7 +341,7 @@ public class AppProfileManager extends MessageHandler {
             synchronized (AppProfileManager.this) {
                 String action = intent.getAction();
                 boolean mode = (boolean)intent.getExtra(Actions.EXTRA_BOOL_MODE);
-                if( Constants.DEBUG_APP_PROFILE ) Slog.i(TAG,"idleChanged mode=" + mode);
+                if( BaikalConstants.BAIKAL_DEBUG_APP_PROFILE ) Slog.i(TAG,"idleChanged mode=" + mode);
                 setDeviceIdleModeLocked(mode);
             }
         }
@@ -310,7 +353,7 @@ public class AppProfileManager extends MessageHandler {
             synchronized (AppProfileManager.this) {
                 String action = intent.getAction();
                 boolean mode = (boolean)intent.getExtra(Actions.EXTRA_BOOL_MODE);
-                if( Constants.DEBUG_APP_PROFILE ) Slog.i(TAG,"chargerChanged mode=" + mode);
+                if( BaikalConstants.BAIKAL_DEBUG_APP_PROFILE ) Slog.i(TAG,"chargerChanged mode=" + mode);
                 setChargerModeLocked(mode);
             }
         }
@@ -322,7 +365,7 @@ public class AppProfileManager extends MessageHandler {
             synchronized (AppProfileManager.this) {
                 String action = intent.getAction();
                 String profile = (String)intent.getExtra("profile");
-                if( Constants.DEBUG_APP_PROFILE ) Slog.i(TAG,"setProfile profile=" + profile);
+                if( BaikalConstants.BAIKAL_DEBUG_APP_PROFILE ) Slog.i(TAG,"setProfile profile=" + profile);
                 setProfileExternalLocked(profile);
             }
         }
@@ -334,7 +377,7 @@ public class AppProfileManager extends MessageHandler {
             synchronized (AppProfileManager.this) {
                 String action = intent.getAction();
                 boolean mode = (boolean)intent.getExtra(Actions.EXTRA_BOOL_MODE);
-                if( Constants.DEBUG_APP_PROFILE ) Slog.i(TAG,"screenChanged mode=" + mode);
+                if( BaikalConstants.BAIKAL_DEBUG_APP_PROFILE ) Slog.i(TAG,"screenChanged mode=" + mode);
                 setScreenModeLocked(mode);
             }
         }
@@ -342,25 +385,28 @@ public class AppProfileManager extends MessageHandler {
 
     private void setHwPerfProfileLocked(String profile, boolean override) {
         if( (!mScreenMode || mIdleProfileActive) && !override ) return;
+        mActivePerfProfile = profile;
         SystemPropertiesSet("baikal.perf.profile", profile);
     }
 
     private void setHwThermProfileLocked(String profile, boolean override) {
         if( (!mScreenMode || mIdleProfileActive) && !override ) return;
+        mActiveThermProfile = profile;
         SystemPropertiesSet("baikal.therm.profile", profile);
     }
 
     private boolean setHwFrameRateLocked(int fps, boolean override) {
-        if( Constants.DEBUG_APP_PROFILE ) Slog.i(TAG,"setHwFrameRateLocked fps=" + fps);
+        if( BaikalConstants.BAIKAL_DEBUG_APP_PROFILE ) Slog.i(TAG,"setHwFrameRateLocked fps=" + fps);
         if( !mVariableFps ) return true;
         if( mIdleProfileActive && !override ) return false;
         Parcel data = Parcel.obtain();
         data.writeInterfaceToken("android.ui.ISurfaceComposer");
         if( fps == -1 || fps == -2 ) { 
-            if( Constants.DEBUG_APP_PROFILE ) Slog.i(TAG,"setHwFrameRateLocked default=" + mDefaultFps);
+            if( BaikalConstants.BAIKAL_DEBUG_APP_PROFILE ) Slog.i(TAG,"setHwFrameRateLocked default=" + mDefaultFps);
             fps = mDefaultFps-1;
         }
-        if( fps < 0 || fps > 4  ) fps = 3;
+        if( fps < 0 || fps > 5  ) fps = 3;
+        if( fps == 5 ) fps = -1;
         data.writeInt(fps);
         try {
             ServiceManager.getService("SurfaceFlinger").transact(1035, data, (Parcel) null, 0);
@@ -376,12 +422,12 @@ public class AppProfileManager extends MessageHandler {
     private void setIdlePerformanceMode(boolean idle) {
         if( mIdleProfileActive == idle ) return;
         mIdleProfileActive = idle;
-        if( Constants.DEBUG_APP_PROFILE ) Slog.i(TAG,"baikal.idle mode=" + idle);
+        if( BaikalConstants.BAIKAL_DEBUG_APP_PROFILE ) Slog.i(TAG,"baikal.idle mode=" + idle);
         SystemPropertiesSet("baikal.idle", idle ? "1" : "0");
         if( !idle ) {
             if( mScreenMode ) {
-                setHwPerfProfileLocked(mActivePerfProfile,true);
-                setHwThermProfileLocked(mActiveThermProfile,true);
+                //setHwPerfProfileLocked(mActivePerfProfile,true);
+                //setHwThermProfileLocked(mActiveThermProfile,true);
             } else {
                 setHwPerfProfileLocked(mScreenOffPerfProfile,true);
                 setHwThermProfileLocked(mScreenOffThermProfile,true);
@@ -393,18 +439,18 @@ public class AppProfileManager extends MessageHandler {
     }
 
     private boolean getIdlePerformanceMode() {
-        return SystemProperties.get("baikal.idle","0").equals("1");
+        return mIdleProfileActive;//SystemProperties.get("baikal.idle","0").equals("1");
     }
 
     private void SystemPropertiesSet(String key, String value) {
-        if( Constants.DEBUG_APP_PROFILE ) {
-            if( Constants.DEBUG_APP_PROFILE ) Slog.d(TAG, "SystemProperties.set("+key+","+value+")");
+        if( BaikalConstants.BAIKAL_DEBUG_APP_PROFILE ) {
+            if( BaikalConstants.BAIKAL_DEBUG_APP_PROFILE ) Slog.d(TAG, "SystemProperties.set("+key+","+value+")");
         }
         try {
             SystemProperties.set(key,value);
         }
         catch( Exception e ) {
-            if( Constants.DEBUG_APP_PROFILE ) Slog.e(TAG, "SystemPropertiesSet: unable to set property "+key+" to "+value);
+            if( BaikalConstants.BAIKAL_DEBUG_APP_PROFILE ) Slog.e(TAG, "SystemPropertiesSet: unable to set property "+key+" to "+value);
         }
     }
 
@@ -461,7 +507,7 @@ public class AppProfileManager extends MessageHandler {
             default:
                 mBrightnessOverride = -1;
         }
-        if( Constants.DEBUG_APP_PROFILE ) Slog.i(TAG,"mBrightnessOverride=" + mBrightnessOverride);
+        if( BaikalConstants.BAIKAL_DEBUG_APP_PROFILE ) Slog.i(TAG,"mBrightnessOverride=" + mBrightnessOverride);
         return mBrightnessOverride;
     }
 
@@ -490,7 +536,7 @@ public class AppProfileManager extends MessageHandler {
          */
         @Override
         public void onCallStateChanged(int state, String incomingNumber) {
-            if( Constants.DEBUG_APP_PROFILE ) Slog.i(TAG,"PhoneStateListener: onCallStateChanged(" + state + "," + incomingNumber + ")");
+            if( BaikalConstants.BAIKAL_DEBUG_APP_PROFILE ) Slog.i(TAG,"PhoneStateListener: onCallStateChanged(" + state + "," + incomingNumber + ")");
 
             synchronized (AppProfileManager.this) {
                 onCallStateChangedLocked(state,incomingNumber);
@@ -506,7 +552,7 @@ public class AppProfileManager extends MessageHandler {
          */
         @Override
         public void onPreciseCallStateChanged(PreciseCallState callState) {
-            if( Constants.DEBUG_APP_PROFILE ) Slog.i(TAG,"PhoneStateListener: onPreciseCallStateChanged(" + callState + ")");
+            if( BaikalConstants.BAIKAL_DEBUG_APP_PROFILE ) Slog.i(TAG,"PhoneStateListener: onPreciseCallStateChanged(" + callState + ")");
             synchronized (AppProfileManager.this) {
                 onPreciseCallStateChangedLocked(callState);
             }
