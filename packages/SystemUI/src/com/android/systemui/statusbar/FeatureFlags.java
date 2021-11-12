d605eb0b293582fb11410bbeb8c967e03de2acbb
shivatejapeddi <shivatejapeddi@gmail.com>
2021-11-12 23:35:02 +0530

    fixes

--- packages/SystemUI/src/com/android/systemui/statusbar/FeatureFlags.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/FeatureFlags.java
@@ -103,11 +103,7 @@ public class FeatureFlags {
 
     /** Whether or not to use the provider model behavior for the status bar icons */
     public boolean isCombinedStatusBarSignalIconsEnabled() {
-        boolean showCombinedStatusBarSignalIcons =
-            Settings.System.getIntForUser(mContext.getContentResolver(),
-                                             Settings.System.SHOW_COMBINED_STATUS_BAR_SIGNAL_ICONS, 0,
-                                             UserHandle.USER_CURRENT) != 0;
-        return mFlagReader.isEnabled(R.bool.flag_combined_status_bar_signal_icons) && showCombinedStatusBarSignalIcons;
+        return mFlagReader.isEnabled(R.bool.flag_combined_status_bar_signal_icons);
     }
 
     /** System setting for provider model behavior */
